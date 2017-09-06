d6971c41867cbf28bb62506064b774a08a379040
isaacg11 <isaacgreywebdeveloper@gmail.com>
2017-09-06 14:08:56 -0700

    update and delete setup via API, no db

--- client/src/App.js
+++ client/src/App.js
@@ -20,15 +20,37 @@ class App extends Component {
     })
   }
 
+  editFriend(id) {
+    let newName = window.prompt('Edit Name');
+    axios.put('/users', {username: newName, id: id}).then((res) => {
+      this.setState({users: res.data});
+    })
+  }
+
+  deleteFriend(id) {
+    axios.delete('/users/' + id).then((res) => {
+      this.setState({users: res.data});
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Friends List</h1>
         <input id='friendName' type='text' />
-        <button onClick={() => this.addFriend(document.getElementById('friendName').value)}>Add Friend</button>
+        <button onClick={
+          () => this.addFriend(document.getElementById('friendName').value)
+        }>Add Friend</button>
         <ul>
           {this.state.users.map(user =>
-            <li key={user.id}>{user.username}</li>
+            <li key={user.id}>{user.username}
+              <button onClick={
+                () => this.editFriend(user.id)
+              }>Edit</button>
+              <button onClick={
+                () => this.deleteFriend(user.id)
+              }>Delete</button>
+            </li>
           )}
         </ul>
       </div>
--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,28 @@ router.post('/', function(req, res, next) {
   res.send(users);
 });
 
+router.put('/', function(req, res, next) {
+  for(let i = 0; i < users.length; i++) {
+    if(users[i].id === req.body.id) {
+      users[i].username = req.body.username;
+    } else {
+      continue;
+    }
+  }
+
+  res.send(users);
+})
+
+router.delete('/:id', function(req, res, next) {
+  for(let i = 0; i < users.length; i++) {
+    if(users[i].id === parseInt(req.params['id'])) {
+      users.splice(i, 1);
+    } else {
+      continue;
+    }
+  }
+
+  res.send(users);
+})
+
 module.exports = router;
